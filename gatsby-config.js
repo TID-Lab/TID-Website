28dd4dccd403c944c0aefdcb1116b719a4beeee4
Matthew Lim <mlim@MacBook-Pro.lfytina2.metronetinc.net>
2021-05-19 19:30:01 -0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  siteMetadata: {
+    title: "T+ID Georgia Tech",
+  },
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+  ],
+};
